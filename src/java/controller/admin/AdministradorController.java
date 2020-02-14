cca392b1ea1e460d00263c60c8940a436547ae57
Marcos Novaes <marcosnovaes@id.uff.br>
2020-02-13 23:14:50 -0300

    admin controller e dao

---
+++ src/java/controller/admin/AdministradorController.java
@@ -0,0 +1,152 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller.admin;
+
+import dao.AdministradorDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Administrador;
+
+/**
+ *
+ * @author marco
+ */
+@WebServlet(name = "AdministradorController", urlPatterns = {"/AdministradorController"})
+public class AdministradorController extends HttpServlet {
+    private final AdministradorDAO adminDAO = new AdministradorDAO();
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+//        try (PrintWriter out = response.getWriter()) {
+//            /* TODO output your page here. You may use following sample code. */
+//            RequestDispatcher rd = request.getRequestDispatcher("/admin/AdminFormJSP.jsp");
+//            rd.forward(request, response);
+//        }
+        String action = request.getServletPath();
+        try {
+            switch (action) {
+            case "/new":
+                showNewForm(request, response);
+                break;
+            case "/insert":
+                insertAdmin(request, response);
+                break;
+            case "/delete":
+                deleteAdmin(request, response);
+                break;
+            case "/edit":
+                showEditForm(request, response);
+                break;
+            case "/update":
+                updateAdmin(request, response);
+                break;
+            default:
+                listAdmin(request, response);
+                break;
+            }
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
+    }
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+//        response.setContentType("text/html;charset=UTF-8");
+//        
+//        String mensagem;
+//        Administrador admin = new Administrador();
+////        if(!request.getParameter("id").isEmpty()){
+////            admin.setId(Integer.parseInt(request.getParameter("id")));
+////        }
+//        admin.setNome(request.getParameter("nome"));
+//        admin.setLogin(request.getParameter("login"));
+//        admin.setSenha(request.getParameter("senha"));
+//        AdministradorDAO adminDao = new AdministradorDAO();
+//        if(adminDao.gravar(admin)){
+//            mensagem = "Admin Salvo com sucesso!";
+//        }
+//        else{
+//            mensagem = "Erro ao gravar admin";
+//        }
+//        request.setAttribute("mensagem", mensagem);
+//        RequestDispatcher rd = request.getRequestDispatcher("/admin/AdminShowJSP.jsp");
+//        rd.forward(request, response);
+          doGet(request, response);
+    }
+    
+    private void listAdmin(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException, ServletException {
+        List<Administrador> listAdmin = adminDAO.listaAdministradores();
+        request.setAttribute("listAdmin", listAdmin);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/admin/AdminList.jsp");
+        dispatcher.forward(request, response);
+    }
+ 
+    private void showNewForm(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/admin/AdminFormJSP.jsp");
+        dispatcher.forward(request, response);
+    }
+ 
+    private void showEditForm(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        Administrador admin = adminDAO.getAdministradorPorID(id);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/admin/AdminFormJSP.jsp");
+        request.setAttribute("admin", admin);
+        dispatcher.forward(request, response);
+ 
+    }
+ 
+    private void insertAdmin(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException {
+        String nome = request.getParameter("nome");
+        String login = request.getParameter("login");
+        String senha = request.getParameter("senha");
+ 
+        Administrador novoAdmin = new Administrador(nome,login, senha);
+        adminDAO.gravar(novoAdmin);
+        response.sendRedirect("list");
+    }
+ 
+    private void updateAdmin(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        String nome = request.getParameter("nome");
+        String login = request.getParameter("login");
+        String senha = request.getParameter("senha");
+ 
+        Administrador admin = new Administrador(id, nome, login, senha);
+        adminDAO.gravar(admin);
+        response.sendRedirect("list");
+    }
+ 
+    private void deleteAdmin(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        adminDAO.excluir(id);
+        response.sendRedirect("list");
+    }
+
+    
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
